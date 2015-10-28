14fba1666e08943f138b91d5c656429172b60ca7
Shane Matthews <shmatthews94@gmail.com>
2015-10-28 17:08:00 -0400

    Map View displayed

---
+++ Milestone1/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  PaceMkr
+//
+//  Created by Shane Matthews on 10/28/15.
+//  Copyright © 2015 Chane Meets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
---
+++ Milestone1/MapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewController.m
+//  PaceMkr
+//
+//  Created by Shane Matthews on 10/28/15.
+//  Copyright © 2015 Chane Meets. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.mapView.showsUserLocation = YES;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
